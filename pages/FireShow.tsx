3cfe42d37ec6b3b73c33f429c1d3f88beaafd395
blacki2016 <Leonard@wieseckel.com>
2025-12-23 01:17:42 +0100

    test

--- pages/FireShow.tsx
+++ pages/FireShow.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { Link } from 'react-router-dom';
 import { Maximize2 } from 'lucide-react';
 import SmartImage from '../components/SmartImage';
 import ScrollToTop from '../components/ScrollToTop';
