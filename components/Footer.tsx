4ba73ec79783173f59123e1824b1b02a9de47d10
Blacki2016 <leonardo.magicano@gmail.com>
2025-12-25 07:37:09 +0100

    test

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Phone, Mail, MapPin, Instagram, Facebook, Youtube, ArrowRight } from 'lucide-react';
+import { Phone, Mail, MapPin, Instagram, Facebook, Youtube } from 'lucide-react';
 
 const Footer: React.FC = () => {
     const currentYear = new Date().getFullYear();
